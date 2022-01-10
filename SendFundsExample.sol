7b54a9f937e32db5411ed195790ce68c4c0411e4
osvlandi <51826605+osvlandi@users.noreply.github.com>
2022-01-10 19:25:13 -0300

    Create SendFundsExample.sol
    Creating a smart contract that can receive and store ether

---
+++ SendFundsExample.sol
@@ -0,0 +1,20 @@
+// SPDX-License-Identifier: GLP-3.0
+
+pragma solidity ^0.8.1;
+
+contract SendMoneyExample {
+
+    uint public balanceReceived;
+
+    function receiveMoney() public payable {
+        balanceReceived += msg.value;
+    }
+
+    function getBalance() public view returns(uint) {
+        return address(this).balance;
+    }
+
+    function withdrawMoneyTo(address payable _to) public {
+        _to.transfer(getBalance());
+    }
+}
