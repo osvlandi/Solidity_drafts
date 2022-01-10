63863b25c160bf58194f98c9502d27b5126b93a6
osvlandi <51826605+osvlandi@users.noreply.github.com>
2022-01-10 19:18:20 -0300

    Create IntergerExample.sol

---
+++ IntergerExample.sol
@@ -0,0 +1,11 @@
+// SPDX-License-Identifier: GPL-3.0
+
+pragma solidity ^0.8.1;
+
+contract IntergerExample {
+    uint public myUint;
+
+      function setMyUint(uint _myUint) public {
+          myUint = _myUint;
+      }
+}
